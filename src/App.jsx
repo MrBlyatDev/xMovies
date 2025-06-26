2c7973faff07dfb47882736f2f3fad553a1da34c
Yerasyl Muratkaliuly <dainsnba@gmail.com>
2025-06-26 16:47:24 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div className="pattern"></div>
+      
+      <div className="wrapper">
+        <header>
+          <h1>Find Movies You'll Enjoy without Hassle</h1>
+        </header>
+      </div>
+    </main>
+  )
+}
+
+export default App
